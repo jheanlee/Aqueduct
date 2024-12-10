085ad1345c8166ee1a8837d428dfc39728503c66
Jhean <jheanlee2008@gmail.com>
2024-12-10 14:52:19 +0800

    [WIP] Domain name support for client

--- client/config.hpp
+++ client/config.hpp
@@ -5,6 +5,7 @@
 #ifndef TUNNEL_CONFIG_HPP
   #define TUNNEL_CONFIG_HPP
   extern const char *host;
+  extern const char *readable_host;
   extern int host_main_port;
 
   extern const char *local_service;
